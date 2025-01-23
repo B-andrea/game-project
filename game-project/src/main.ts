00d7ad4a59e817ac40337410767076827c6945ef
BCruzMejia <BCruzMejia@bs-macbook-pro.lan>
2025-01-23 19:54:25 +0000

    added query selectors

--- game-project/src/main.ts
+++ game-project/src/main.ts
@@ -1,1 +1,44 @@
 import './style.css'
+
+
+const myTTTArr = new Array(9).fill([""])
+
+
+// Selecting HTML element from our page
+const boxBtns = document.querySelectorAll<HTMLButtonElement>(".box")
+const statusDisplay = document.querySelector<HTMLHeadingElement>("#statusDisplay")
+const startBtn = document.querySelector<HTMLButtonElement>("#startBtn")
+const resetBtn = document.querySelector<HTMLButtonElement>("#resetBtn")
+
+const handleBoxClick = (event: Event, player: any) => {
+
+
+    const box = event.currentTarget as HTMLButtonElement
+    if (box) {
+        box.innerHTML = player
+    }
+
+}
+
+const playGame = (e: Event) => {
+    let currentPlayer = ""
+
+    // Decide rules on who is starting
+    currentPlayer = "O"
+
+    if ((currentPlayer === "O")) {
+        handleBoxClick(e, currentPlayer)
+        // After player O finished reassigned currentplayer
+        // currentPlayer = 
+    }
+    if (currentPlayer === "X") {
+
+    }
+}
+
+
+boxBtns.forEach((box) => {
+    box.addEventListener('click', (event: Event) => {
+        handleBoxClick(event, "X")
+    })
+})
