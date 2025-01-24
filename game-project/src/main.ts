2d75bd02a8463faec5ced7f1432a8579fcbac4d3
BCruzMejia <BCruzMejia@Bs-MacBook-Pro.cust.communityfibre.co.uk>
2025-01-24 12:10:16 +0000

    implemented alternating players

--- game-project/src/main.ts
+++ game-project/src/main.ts
@@ -1,8 +1,16 @@
 import './style.css'
 
+/* Need to define the winning set of combinations
+Before a player is able to take their turn, need to check if they are able to.
+Meaning
+- The current state of the game doesn't yet have a winning combination
+- The current state of the game isn't tied (all boxes filled with no winning combination)
+*/
 
-const myTTTArr = new Array(9).fill([""])
 
+
+const gameState = new Array(9).fill([""]);
+let currentPlayer = "X";
 
 // Selecting HTML element from our page
 const boxBtns = document.querySelectorAll<HTMLButtonElement>(".box")
@@ -10,15 +18,20 @@ const statusDisplay = document.querySelector<HTMLHeadingElement>("#statusDisplay
 const startBtn = document.querySelector<HTMLButtonElement>("#startBtn")
 const resetBtn = document.querySelector<HTMLButtonElement>("#resetBtn")
 
-const handleBoxClick = (event: Event, player: any) => {
 
-
+const handleBoxClick = (event: Event) => {
     const box = event.currentTarget as HTMLButtonElement
     if (box) {
-        box.innerHTML = player
+        box.innerHTML = currentPlayer
+        box.classList.add("played");
+        currentPlayer = currentPlayer === "X" ? "O" : "X"
     }
-
 }
+
+boxBtns.forEach((box) => {
+    box.addEventListener('click', handleBoxClick)
+})
+
 
 const playGame = (e: Event) => {
     let currentPlayer = ""
@@ -27,18 +40,11 @@ const playGame = (e: Event) => {
     currentPlayer = "O"
 
     if ((currentPlayer === "O")) {
-        handleBoxClick(e, currentPlayer)
+        handleBoxClick(e)
         // After player O finished reassigned currentplayer
-        // currentPlayer = 
+        // currentPlayer =
     }
     if (currentPlayer === "X") {
 
     }
 }
-
-
-boxBtns.forEach((box) => {
-    box.addEventListener('click', (event: Event) => {
-        handleBoxClick(event, "X")
-    })
-})
