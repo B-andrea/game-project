51df833a35eb4f149be8859a8c3b6b5bd8687c06
BCruzMejia <BCruzMejia@Bs-MacBook-Pro.cust.communityfibre.co.uk>
2025-01-26 15:28:28 +0000

    added check winner function

--- game-project/src/main.ts
+++ game-project/src/main.ts
@@ -7,9 +7,7 @@ Meaning
 - The current state of the game isn't tied (all boxes filled with no winning combination)
 */
 
-
-
-const gameState = new Array(9).fill([""]);
+let gameState = new Array(9).fill([""]);
 let currentPlayer = "X";
 
 // Selecting HTML element from our page
@@ -21,8 +19,15 @@ const resetBtn = document.querySelector<HTMLButtonElement>("#resetBtn")
 
 const handleBoxClick = (event: Event) => {
     const box = event.currentTarget as HTMLButtonElement
+    const index = Array.from(boxBtns).indexOf(box);
+    // convert boxBtns into an array to use array methods and find the index of clicked cell
+    if (gameState[index] === "") {
+        // update the sameState array with the current player's symbol
+        gameState[index] === currentPlayer;
+    }
     if (box) {
         box.innerHTML = currentPlayer
+        // display the current players symbol on the clicked cell
         box.classList.add("played");
         currentPlayer = currentPlayer === "X" ? "O" : "X"
         statusDisplay.textContent = `${currentPlayer}'s turn`;
@@ -33,19 +38,29 @@ boxBtns.forEach((box) => {
     box.addEventListener('click', handleBoxClick)
 })
 
+const checkWinner = () => {
+    const winCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    // Loop through each win combination
+    for (let i = 0; i < winCombination.length; i++) {
+        // destructure the current combination into a, b, and c
+        const [a, b, c] = winCombination[i];
 
-const playGame = (e: Event) => {
-    let currentPlayer = ""
+        if (gameState[a] !== '' && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+            statusDisplay.textContent = `${currentPlayer} wins!`;
+            return;
+            // exit function after a winner is found
 
-    // Decide rules on who is starting
-    currentPlayer = "O"
-
-    if ((currentPlayer === "O")) {
-        handleBoxClick(e)
-        // After player O finished reassigned currentplayer
-        // currentPlayer =
+        }
     }
-    if (currentPlayer === "X") {
 
+    if (!gameState.includes("")) {
+        statusDisplay.textContent = "It's a draw!";
     }
-}
+
+};
+
+
+
+
+// const playGame = (e: Event) => {
+//     let currentPlayer = ""
